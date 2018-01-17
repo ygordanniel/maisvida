2b628278a2f7f1b772a49140ca81cf36c20707f8
Ygor Azevedo <ygor.azevedo@basis.com.br>
2018-01-17 19:45:02 -0200

    CORS configuration bean creation to accept request from different domains

--- codigo-fonte/servico/src/main/java/br/med/maisvida/security/WebSecurityConfig.java
+++ codigo-fonte/servico/src/main/java/br/med/maisvida/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import br.med.maisvida.security.jwt.JWTLoginFilter;
 import br.med.maisvida.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,6 +14,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +31,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Qualifier("CustomUserDetailsService")
     private UserDetailsService userDetailsService;
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT"));
+        configuration.addAllowedHeader("Authorization");
+        configuration.addAllowedHeader("Content-Type");
+        configuration.addExposedHeader("Authorization");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
@@ -33,7 +52,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
 
             .addFilterBefore(new JWTLoginFilter("/login", authenticationManager(), usuarioService), UsernamePasswordAuthenticationFilter.class)
-            .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+            .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+            .cors();
     }
 
     @Override
